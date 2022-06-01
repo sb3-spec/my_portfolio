92b24ec1b243dac1cd2685742f94844013130221
Robert Sheffield <sheffira2@gmail.com>
2022-05-31 19:18:50 -0700

    Netlify build failing do to unused import pdfFiller

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
 import "./Contact.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import Popup from "../components/Popup";
